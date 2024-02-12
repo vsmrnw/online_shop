244a140cd24dc0d475e483197b2797a0eb312f51
vsmrnw <vsevolod.smirnov@gmail.com>
2024-02-12 21:02:25 +0300

    add views to product & product detail

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -15,6 +16,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def _get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
 
 class Product(models.Model):
@@ -41,3 +46,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def _get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
