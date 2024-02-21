dc557ff0ceb16a682d7da698e3ce067245485ddd
vsmrnw <vsevolod.smrnv@gmail.com>
2024-02-21 18:56:34 +0300

    add links to orders in stripe dashboard

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from orders.models import OrderItem, Order
 
+from django.utils.safestring import mark_safe
+
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
+order_stripe_payment.short_description = 'Stripe payment'
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -12,6 +25,6 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    order_stripe_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
