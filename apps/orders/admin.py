f103f16ecc6f898543c86bdd2231b100cd6bfe65
vsmrnw <vsevolod.smrnv@gmail.com>
2024-02-23 13:10:58 +0300

    add custom views to admin site

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import OrderItem, Order
 
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 
 def order_stripe_payment(obj):
@@ -50,11 +51,17 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    order_stripe_payment, 'created', 'updated']
+                    order_stripe_payment, 'created', 'updated',
+                    order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 from cart.cart import Cart
@@ -27,3 +28,11 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
