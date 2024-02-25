5b3b0fef30aeb546e401200da3e3204d6b4e0cea
vsmrnw <vsevolod.smrnv@gmail.com>
2024-02-25 16:15:28 +0300

    add recommended goods in cart view

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 
 from coupons.forms import CouponApplyForm
 from shop.models import Product
+from shop.recomender import Recommender
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -35,6 +36,15 @@ def cart_detail(request):
             'quantity': item['quantity'],
             'override': True})
     coupon_apply_form = CouponApplyForm()
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if (cart_products):
+        recommended_products = r.suggest_products_for(
+            cart_products, max_results=4
+        )
+    else:
+        recommended_products = []
     return render(request, 'cart/detail.html',
                   {'cart': cart,
-                   'coupon_apply_form': coupon_apply_form})
+                   'coupon_apply_form': coupon_apply_form,
+                   'recommended_products': recommended_products})
